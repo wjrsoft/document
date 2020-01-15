34105d001199927dda7427481185448c515eff98
wjrsoft <wjrsoftware@163.com>
2020-01-15 22:25:22 +0800

    修改成war包

--- src/main/java/com/wsm/document/DocumentApplication.java
+++ src/main/java/com/wsm/document/DocumentApplication.java
@@ -1,11 +1,20 @@
 package com.wsm.document;
 
+import javax.ws.rs.core.Application;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DocumentApplication {
+public class DocumentApplication extends SpringBootServletInitializer{
 
+	 @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(Application.class);
+	  }
+	 
     public static void main(String[] args) {
         SpringApplication.run(DocumentApplication.class, args);
     }
